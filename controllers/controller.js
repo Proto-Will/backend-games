bd01a696db9d8617870ccb24b09b16c0abe3ad5e
will <willshakeyspeare@gmail.com>
2022-07-04 15:57:33 +0100

    [3. GET api/categories] removes console log

--- controllers/controller.js
+++ controllers/controller.js
@@ -3,7 +3,8 @@ require("../models/model")
 
 getCategories = (req, res) => {
   selectAllCategories().then((categories) => {
-    console.log(categories)
       res.status(200).send({ categories })
     })
 }
+
+
